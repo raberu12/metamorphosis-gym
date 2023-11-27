d71b97d59cc517f92d5dde3983322b3f4ca8bbdc
Jedd <christian.juab025@gmail.com>
2023-11-27 14:06:26 +0800

    initial sidebar

---
+++ version-tailwind/src/Components/Sidebar.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+    function Sidebar() {
+
+        const sidebarLinks = [
+            {to: '/OverviewPage', text: 'Overview'},
+            {to: '/MembershipPage', text: 'Membership'},
+            {to: '/ConsultationPage', text: 'Consultation'},
+            {to: '/EmailalertsPage', text: 'Email Alerts'},
+            {to: '/SettingsPage', text: 'Settings'}
+        ]
+
+        const sidebarIcon = {
+            OverviewPage: 'overviewicon.png',
+            MembershipPage: 'membership.png',
+            ConsultationPage: 'consultation.png',
+            EmailalertsPage: 'emailalert.png',
+            SettingsPage: 'settings.png'
+        }
+
+        const personName = [
+            {name: 'Jedd Juab'},
+        ]
+
+        
+
+        
+
+
+        return(
+                <div className='bg-dark-elixir h-screen w-52 fixed text-white '>
+                    <div className='text-center flex flex-col'>
+                        <img src='icon.png' alt= 'profile pic' className='ml-14 mt-8 mb-20 w-24'/>
+                        <h2 className='mb-4'>{personName[0].name}</h2>
+                        <ul>
+                            {sidebarLinks.map((link, index) => (
+                                <li
+                                    key={index}
+                                    className='hover:bg-sidebar cursor-pointer flex items-center'
+                                >
+                                    <img
+                                        src={sidebarIcon[link.to.replace('/', '')]}
+                                        alt={`${link.text} icon`}
+                                        className='w-8 h-8 ml-4 mt-3' 
+                                    />
+                                    
+                                    <Link 
+                                        to={link.to} 
+                                        className='text-center ml-2'
+                                        activeClassName='active bg-sidebar'
+                                    >   
+                                        {link.text}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className=' fixed mt-96 ml-12'>
+                        <button 
+                            className='bg-orangish w-32 h-12 rounded-lg cursor-pointer text-xl'>
+                                {/* <img src='log-out.png' className='absolute ml-4 w-4 h-4'></img> */}
+                                Log out
+                        </button>
+                    </div>
+                </div>
+        )
+    }
+
+
+    export default Sidebar
